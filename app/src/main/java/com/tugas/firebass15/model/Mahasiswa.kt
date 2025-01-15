3a4d0ccc3352f254a96c16c2fab5854a1eb4643e
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-01-15 15:52:47 +0700

    menambahkan data judulskripsi dan dosen pembimbing

--- app/src/main/java/com/tugas/firebass15/model/Mahasiswa.kt
+++ app/src/main/java/com/tugas/firebass15/model/Mahasiswa.kt
@@ -6,7 +6,10 @@ data class Mahasiswa(
     val alamat: String,
     val jeniskelamin: String,
     val kelas: String,
-    val angkatan: String
+    val angkatan: String,
+    val judulSkripsi: String,
+    val dosenPembimbing1: String,
+    val dosenPembimbing2: String
 ){
-    constructor() : this("", "", "", "", "", "")
+    constructor() : this("", "", "", "", "", "", "", "", "")
 }
